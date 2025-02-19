b86b6313f484b24562fcd97d8ede99817796effa
Ralfs3glitis <ralfs.eglitis123@gmail.com>
2025-02-19 10:31:58 +0200

    ADDED student setters and  getters

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,12 +1,12 @@
 package model;
 
 public class Professor {
-	private long p_ID = 0;
+	private long pID;
 	private String name = "";
 	private String surname = "";
 	private String degree = "";
 	public Professor(String x, String y, String z) {
-		p_ID += 1;
+		pID += 1;
 		name = x;
 		surname = y;
 		degree = z;
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,54 @@
 package model;
 
 public class Student {
-
+	//1. mainigie
+	private long stID;
+	private String name;
+	private String surname;
+	
+	//getters
+	public long getStID() {
+		return stID;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	
+	//setters
+	/*
+	public void setStID(long InputID) {
+		stID = InputID;
+	}
+	*/
+	public void setName(String InputName) {
+		if(InputName != null){
+			name = InputName;
+		}
+		else {
+			name = "unknown";
+		}
+	}
+	public void setSurname(String InputSname) {
+		if(InputSname != null){
+			surname = InputSname;
+		}
+		else {
+			surname = "unknown";
+		}
+	}
+	
+	// bez argumenta konstruktors
+	
+	
+	//konstruktors ar argumentiem
+	public Student(String x, String y) {
+		stID += 1;
+		name = x;
+		surname = y;
+	}
+	
 }
+
