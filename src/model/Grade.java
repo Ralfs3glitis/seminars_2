b66915e74ba167b0d8956d244825a5b835695ed5
Ralfs3glitis <ralfs.eglitis123@gmail.com>
2025-02-21 09:55:54 +0200

    EDIT: added setters, getters to COURSE class

---
+++ src/model/Grade.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Grade {
+	private long gID;
+	private int grValue;
+	private Student student;
+	private Course course;
+	private int counter = 0;
+	
+	
+	public long getgID() {
+		return gID;
+	}
+	public void setgID() {
+		gID = counter++;
+	}
+	
+	public int getGrValue() {
+		return grValue;
+	}
+	public void setGrValue(int grValue) {
+		if(grValue > 0 && grValue <= 10) {
+			this.grValue = grValue;
+		}
+		else {
+			this.grValue = 0;
+		}
+	}
+	
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		if(student != null) {
+			this.student = student;
+		}
+		else {
+			this.student = new Student();
+		}
+	}
+	
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		if(course != null) {
+			this.course = course;
+		}
+		else {
+			this.course = new Course();
+		}
+	}
+	
+	
+	
+	
+	
+	
+}
